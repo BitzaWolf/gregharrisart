a2cc6f5895adc5d19ec6d1223fc131724569be66
Anthony 'Bitzawolf' Pepe <bitzah@gmail.com>
2017-10-10 12:09:25 -0700

    Cleaned up Album page

--- themes/may_2017/album.php
+++ themes/may_2017/album.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 // force UTF-8 Ø
 
 if (!defined('WEBPATH'))
@@ -6,86 +6,61 @@ if (!defined('WEBPATH'))
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="<?php echo LOCAL_CHARSET; ?>">
-		<?php zp_apply_filter('theme_head'); ?>
-		<?php printHeadTitle(); ?>
-		<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
-		<link rel="stylesheet" href="<?php echo pathurlencode(dirname(dirname($zenCSS))); ?>/common.css" type="text/css" />
-		<?php if (class_exists('RSS')) printRSSHeaderLink('Album', getAlbumTitle()); ?>
-	</head>
-	<body>
-		<?php zp_apply_filter('theme_body_open'); ?>
-		<div id="main">
-			<div id="gallerytitle">
-				<?php
-				if (getOption('Allow_search')) {
-					printSearchForm();
-				}
-				?>
-				<h2>
-					<span>
-						<?php printHomeLink('', ' | '); printGalleryIndexURL(' | ', getGalleryTitle()); printParentBreadcrumb(); ?>
-					</span>
-					<?php printAlbumTitle(); ?>
-				</h2>
-			</div>
-			<div id="padbox">
-				<?php printAlbumDesc(); ?>
-				<div id="albums">
-					<?php while (next_album()): ?>
-						<div class="album">
-							<div class="thumb">
-								<a href="<?php echo html_encode(getAlbumURL()); ?>" title="<?php echo gettext('View album:'); ?> <?php printAnnotatedAlbumTitle(); ?>"><?php printAlbumThumbImage(getAnnotatedAlbumTitle()); ?></a>
-							</div>
-							<div class="albumdesc">
-								<h3><a href="<?php echo html_encode(getAlbumURL()); ?>" title="<?php echo gettext('View album:'); ?> <?php printAnnotatedAlbumTitle(); ?>"><?php printAlbumTitle(); ?></a></h3>
-								<small><?php printAlbumDate(""); ?></small>
-								<div><?php printAlbumDesc(); ?></div>
-							</div>
+<head>
+	<meta charset="<?php echo LOCAL_CHARSET; ?>">
+	<?php zp_apply_filter('theme_head'); ?>
+	<?php printHeadTitle(); ?>
+	<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
+	<link rel="stylesheet" href="<?php echo pathurlencode(dirname(dirname($zenCSS))); ?>/common.css" type="text/css" />
+	<?php if (class_exists('RSS')) printRSSHeaderLink('Album', getAlbumTitle()); ?>
+</head>
+<body>
 
-						</div>
-					<?php endwhile; ?>
-				</div>
-				<br class="clearfloat">
-				<div id="images">
-					<?php while (next_image()): ?>
-						<div class="image">
-							<div class="imagethumb">
-								<a href="<?php echo html_encode(getImageURL()); ?>" title="<?php printBareImageTitle(); ?>">
-									<?php printImageThumb(getAnnotatedImageTitle()); ?>
-								</a>
-							</div>
-						</div>
-					<?php endwhile; ?>
-				</div>
-				<br class="clearfloat">
-				<?php
-    printPageListWithNav("« " . gettext("prev"), gettext("next") . " »");
-    if (function_exists('printAddToFavorites')) printAddToFavorites($_zp_current_album);
-    printTags('links', gettext('<strong>Tags:</strong>') . ' ', 'taglist', '');
-    @call_user_func('printGoogleMap');
-    @call_user_func('printSlideShowLink');
-    @call_user_func('printRating');
-    @call_user_func('printCommentForm');
-    ?>
-			</div>
-		</div>
-		<div id="credit">
-			<?php
-			if (function_exists('printFavoritesURL')) {
-				printFavoritesURL(NULL, '', ' | ', '<br />');
-			}
-			?>
-			<?php
-   if (class_exists('RSS')) printRSSLink('Album', '', gettext('Album RSS'), ' | ');
-   printCustomPageURL(gettext("Archive View"), "archive", '', '', ' | ');
-   printZenphotoLink();
-   @call_user_func('printUserLogin_out', " | ");
-   ?>
-		</div>
-		<?php
-		zp_apply_filter('theme_body_close');
-		?>
-	</body>
+
+<? zp_apply_filter('theme_body_open'); ?>
+<header>
+    <span class="site-header-title">Greg Harris Art</span>
+	<nav>
+		<a href="/Digital-Art/">Digital Art</a>
+		<a href="/Sculptures/">Sculptures</a>
+		<a href="/Featured/">Featured</a>
+	</nav>
+</header>
+
+
+<div class="sub-albums">
+	<? while(next_album()): ?>
+		<? printAlbumThumbImage(getAnnotatedAlbumTitle()); ?>
+	<? endwhile; ?>
+</div>
+
+
+<div class="images">
+	<? while(next_image()): ?>
+		<? printImageThumb(getAnnotatedImageTitle()); ?>
+	<? endwhile; ?>
+</div>
+
+
+<div class="pagination">
+	<? printPageListWithNav("« " . gettext("prev"), gettext("next") . " »"); ?>
+</div>
+
+
+<footer>
+	<div class="copyright">
+		&copy; Greg Harris  <? echo date("Y"); ?>
+	</div>
+	<div class="bitzawolf">
+		<a href="http://bitzawolf.com">Website Design by Bitzawolf</a>
+	</div>
+</footer>
+
+
+<? @call_user_func('mobileTheme::controlLink'); ?>
+<? @call_user_func('printLanguageSelector'); ?>
+<? zp_apply_filter('theme_body_close'); ?>
+
+
+</body>
 </html>
